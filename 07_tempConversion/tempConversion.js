6633336fc0aa67088cd7947a68c61b3bd33deb0b
mateusz3225 <mateusz3225@gmail.com>
2022-07-24 16:50:25 +0200

    task 07 tempConversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(faren) {
+farenToCel= (faren-32)*0.555555556;
+farenToCel=Math.round((farenToCel*10)) /10;
+return farenToCel;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  celtoFaren= (cel*1.8)+32;
+  celtoFaren=Math.round((celtoFaren*10)) /10;
+  return celtoFaren;
 };
 
 // Do not edit below this line
