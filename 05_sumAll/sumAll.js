5ed7367cf9fcaab1332390be4e254dddb7838b2e
mateusz3225 <mateusz3225@gmail.com>
2022-07-24 16:36:06 +0200

    task 05 sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    if (first<0 || last<0 || typeof first != 'number' || typeof last != 'number')
+    {
+        return "ERROR";
+    };
+    if (first>last){[first,last]=[last,first];};
+let number = 0;
+for (i=first;i<=last;i++){
+    number += i;
 
+}
+return number;
 };
 
 // Do not edit below this line
