f01f2d97dd05d011ea590f13daff7ad887219502
mateusz3225 <mateusz3225@gmail.com>
2022-07-24 15:53:14 +0200

    task 03 reverseString complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+let reversed= '';
+{ 
+    for (i=1;i<=string.length;i++)
+    {
+        
+         reversed += string.substr(-i,1);
+    } 
+    return reversed;
+}
 };
 
 // Do not edit below this line
