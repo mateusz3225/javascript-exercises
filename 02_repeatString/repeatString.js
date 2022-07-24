103f03a2bf4c00b96cb5db9ce864540318e90466
mateusz3225 <mateusz3225@gmail.com>
2022-07-24 15:37:28 +0200

    complete task 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
+const stringstart=string;
+    for (i=1;i<num;i++){
+        string += stringstart;
+    }
+
+    if(num==0)
+    {return '';} 
+        else if (num<0)
+        {return 'ERROR';}
+            else 
+            {return string;}
 };
 
 // Do not edit below this line
