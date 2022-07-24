24d238d637ff68f81640551584f7a0d3ffb48290
mateusz3225 <mateusz3225@gmail.com>
2022-07-24 16:18:14 +0200

    task 04 removeFromArray done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
 
+for (x=1;x<=arguments.length;x++){
+
+    for (i=0;i<=array.length;i++)
+    {
+        if (array[i]===arguments[x])
+        {
+        array.splice(i,1)
+        }
+    } 
+    
+} return array;
 };
 
 // Do not edit below this line
